d8709d30ae57ffd8621bf8e730fafd4d013819d6
Arto Eskelinen <arto.eske@gmail.com>
2023-08-07 10:30:26 +0300

    pelaajien määrän tiedustelun runko

--- toiminnot.js
+++ toiminnot.js
@@ -1,1 +1,10 @@
-console.log("mjoo täällä ollaan")
+document.getElementById("pelaajatOk").addEventListener("click", testi);
+
+function testi(event){
+    event.preventDefault();
+    if(document.getElementById("pelaajienLkm").value < 2){
+        console.log("LISÄÄ PELAAJIA!!!");
+    } else {
+        console.log("kaikki ookoo");
+    }
+}
