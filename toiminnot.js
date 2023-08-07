d43cd524d1f48d3c9f024d0f8b2cd0952c420966
Arto Eskelinen <arto.eske@gmail.com>
2023-08-07 10:58:32 +0300

    pelaajamäärän tarkistus

--- toiminnot.js
+++ toiminnot.js
@@ -4,7 +4,9 @@ function testi(event){
     event.preventDefault();
     if(document.getElementById("pelaajienLkm").value < 2){
         console.log("LISÄÄ PELAAJIA!!!");
+        document.getElementById("pelaajienLkm").value = "";
     } else {
         console.log("kaikki ookoo");
+        document.getElementById("alkuPelaajat").style.display = "none";
     }
 }
