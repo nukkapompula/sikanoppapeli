39633042f3ec6a1d4d303151d6829ba9769f38dd
Arto Eskelinen <arto.eske@gmail.com>
2023-08-07 13:50:27 +0300

    pelaajien nimeäminen

--- toiminnot.js
+++ toiminnot.js
@@ -1,12 +1,27 @@
-document.getElementById("pelaajatOk").addEventListener("click", testi);
+document.getElementById("pelaajatOk").addEventListener("click", pelaajaMaara);
+var pelaajat = 0;
 
-function testi(event){
+function pelaajaMaara(event){
     event.preventDefault();
     if(document.getElementById("pelaajienLkm").value < 2){
         console.log("LISÄÄ PELAAJIA!!!");
         document.getElementById("pelaajienLkm").value = "";
     } else {
         console.log("kaikki ookoo");
+        pelaajat = document.getElementById("pelaajienLkm").value;
         document.getElementById("alkuPelaajat").style.display = "none";
+        document.getElementById("alkuNimet").style.display = "block";
+        nimeaPelaajat();
+    }
+}
+
+function nimeaPelaajat(){
+    for(let indeksi=0; indeksi<pelaajat; indeksi++){
+        let uusiElementti = document.createElement("p");
+        let uusiElementtiNimi = document.createTextNode(`Pelaajan ${indeksi+1} nimi: `);
+        let uusiSyote = document.createElement("input");
+        uusiElementti.appendChild(uusiElementtiNimi);
+        uusiElementti.appendChild(uusiSyote);
+        document.getElementById("alkuNimet").appendChild(uusiElementti);
     }
 }
