b9815f3ccffeafab6fc95db3f202becbda0551ab
Arto Eskelinen <arto.eske@gmail.com>
2023-08-08 10:30:10 +0300

    asetusten tarkistus alulle

--- toiminnot.js
+++ toiminnot.js
@@ -1,8 +1,11 @@
 document.getElementById("pelaajatOk").addEventListener("click", pelaajaMaara);
+document.getElementById("asetuksetOk").addEventListener("click", tarkistaAsetukset);
 var pelaajat = 0;
+var valmistelutOk = false;
+var maxPisteet = 0;
 
 function pelaajaMaara(event){
-    // pelaaja syöttää pelaajien lukumäärän
+    // käyttäjän syöttäessä pelaajien lukumäärän asetukset ilmestyvät näkyviin
     event.preventDefault();
     if(document.getElementById("pelaajienLkm").value < 2){
         document.getElementById("pelaajienLkm").value = "";
@@ -24,5 +27,16 @@ function nimeaPelaajat(){
         uusiElementti.appendChild(uusiElementtiNimi);
         uusiElementti.appendChild(uusiSyote);
         document.getElementById("alkuNimet").appendChild(uusiElementti);
+    }
+}
+
+function tarkistaAsetukset(event){
+    event.preventDefault();
+    if(document.getElementById("voittoPisteet").value < 6){
+        console.log("ei passaa, patu");
+        valmistelutOk = false;
+        document.getElementById("voittoPisteet").value = "";
+    } else {
+        valmistelutOk = true;
     }
 }
