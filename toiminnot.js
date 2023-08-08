485c1c54382e39bf4f29a5ce8bf88e52b14c4b81
Arto Eskelinen <arto.eske@gmail.com>
2023-08-08 09:56:02 +0300

    alkuasetukset siistimmäksi

--- toiminnot.js
+++ toiminnot.js
@@ -2,20 +2,20 @@ document.getElementById("pelaajatOk").addEventListener("click", pelaajaMaara);
 var pelaajat = 0;
 
 function pelaajaMaara(event){
+    // pelaaja syöttää pelaajien lukumäärän
     event.preventDefault();
     if(document.getElementById("pelaajienLkm").value < 2){
-        console.log("LISÄÄ PELAAJIA!!!");
         document.getElementById("pelaajienLkm").value = "";
     } else {
-        console.log("kaikki ookoo");
         pelaajat = document.getElementById("pelaajienLkm").value;
         document.getElementById("alkuPelaajat").style.display = "none";
-        document.getElementById("alkuNimet").style.display = "block";
+        document.getElementById("asetukset").style.display = "block";
         nimeaPelaajat();
     }
 }
 
 function nimeaPelaajat(){
+    // luodaan pelaajia vastaava määrä täytettäviä nimikenttiä
     for(let indeksi=0; indeksi<pelaajat; indeksi++){
         let uusiElementti = document.createElement("p");
         let uusiElementtiNimi = document.createTextNode(`Pelaajan ${indeksi+1} nimi: `);
@@ -25,12 +25,4 @@ function nimeaPelaajat(){
         uusiElementti.appendChild(uusiSyote);
         document.getElementById("alkuNimet").appendChild(uusiElementti);
     }
-    let uusiOhje = document.createElement("p");
-    let uusiOhjeTeksti = document.createTextNode("Pelin voittamiseen vaadittava pistemäärä: ");
-    let pisteSyote = document.createElement("input");
-    pisteSyote.id = "voittoPisteet";
-    pisteSyote.type = "number";
-    uusiOhje.appendChild(uusiOhjeTeksti);
-    uusiOhje.appendChild(pisteSyote);
-    document.getElementById("alkuNimet").appendChild(uusiOhje);
 }
